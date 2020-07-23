dc48f479455fc4584fa4efa6d5ef5315fd29117d
Vishal Khandelwal <vishalkhandelwaal@gmail.com>
2020-07-23 20:56:56 +0530

    added files

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
+from datetime import datetime
+import numpy as np
+import sklearn
+
+app = Flask(__name__)
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Years = int(datetime.now().year) - Year
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='Petrol'):
+                Fuel_Type_Petrol=1
+                Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol=='Diesel'):
+                Fuel_Type_Petrol=0
+                Fuel_Type_Diesel=1
+        elif(Fuel_Type_Petrol=='CNG'):
+                Fuel_Type_Petrol=0
+                Fuel_Type_Diesel=0
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+        else:
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+        else:
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Years,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {} L".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
